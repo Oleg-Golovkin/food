d755124317a8a5fca2a98f33ceffcd88000db399
Oleg <olegjan9@rambler.ru>
2021-12-01 21:41:27 +0300

    01.12.2021

---
+++ server.php
@@ -0,0 +1,5 @@
+<?php
+// Позволяет работать с форматом json
+// $_POST = json_decode(file_get_contents("php://input"), true);
+// Показывает какие данные закидываем на сервер
+echo var_dump($_POST);
