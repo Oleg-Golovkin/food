f6fa855af5e402ea24331ce68b0fdbb1170ba66e
Oleg <olegjan9@rambler.ru>
2021-12-10 22:06:51 +0300

    10.12.2021

--- js/script.js
+++ js/script.js
@@ -44,82 +44,105 @@ document.addEventListener("DOMContentLoaded", () => {
         current = document.querySelector("#current"),
         total = document.querySelector("#total");
 
-    function scrollSlide() {
+    let indexSlide = 1;
+
+    scrollSlide(indexSlide);
+
+    function scrollSlide(n) {
+        if (n > slide.length) {
+            indexSlide = 1;
+        }
+
+        if (n < 1) {
+            indexSlide = slide.length;
+        }
+        slide.forEach(item => {
+            item.style.display = "none"; /* удаляем весь контент */
+        });
+
+        slide[indexSlide - 1].style.display = "block"; /* добавляем один из по порядку */
         
-        function hideSlide() {
-            slide.forEach(item => {
-                item.style.display = "none"; /* удаляем весь контент */
-            });
-        }
-    
-        function showSlide(i = 0) {
-            slide[i].style.display = "block"; /* добавляем один из по порядку */
-        }
-        hideSlide();
-        showSlide();
-
-
-        slide.forEach((item, c) => {
-            total.textContent = `${0}${c + 1}`;
-        });
-        let i = 0,
-            /* переменная по прокрутке изображения */
-            b = 1;
-        /* переменная по простановке цифры, отсчитывающей
-               изображение. Цифра не 0, чтобы было более привычней. */
-        current.textContent = "01";
-
-        sliderCounter.addEventListener("click", (e) => {
-            if (e.target && e.target.matches(`${".offer__slider-next"}, ${".offer__slide__next__img"}`)) {
-
-                if (i == 3) {
-                    i = 0;
-                    b = 1;
-                    current.textContent = `${0}${b}`;
-                    hideSlide();
-                    showSlide(i);
-                    sliderCounter.removeEventListener("click", (e));
-                } else if (i <= 3) {
-                    i++;
-                    b++;                   
-                    if (i < 10) {
-                        current.textContent = `${0}${b}`;
-                    } else {
-                        current.textContent = b;
-                    }
-                    hideSlide();
-                    showSlide(i);
-                }
-            }
-
-            if (e.target && e.target.matches(`${".offer__slider-prev"}, ${".offer__slide__prev__img"}`)) {
-
-                if (i == 0) {
-                    i = 3;
-                    b = 4;
-                    current.textContent = `${0}${b}`;
-                    hideSlide();
-                    showSlide(i);
-                    sliderCounter.removeEventListener("click", (e));
-                } else if (i <= 3) {
-                    i--;
-                    b--;
-                    
-                    if (i < 10) {
-                        current.textContent = `${0}${b}`;
-                    } else {
-                        current.textContent = b;
-                    }
-                                    
-                    hideSlide();
-                    showSlide(i);
-                }
-            }
-        });
-
-
     }
-    scrollSlide();
+    
+
+
+
+
+
+
+
+
+    // function hideSlide() {
+    //     slide.forEach(item => {
+    //         item.style.display = "none"; /* удаляем весь контент */
+    //     });
+    // }
+
+    // function showSlide(i = 0) {
+    //     slide[i].style.display = "block"; /* добавляем один из по порядку */
+    // }
+    // hideSlide();
+    // showSlide();
+
+
+    // slide.forEach((item, c) => {
+    //     total.textContent = `${0}${c + 1}`;
+    // });
+    // let i = 0,
+    //     /* переменная по прокрутке изображения */
+    //     b = 1;
+    // /* переменная по простановке цифры, отсчитывающей
+    //        изображение. Цифра не 0, чтобы было более привычней. */
+    // current.textContent = "01";
+
+    // sliderCounter.addEventListener("click", (e) => {
+    //     if (e.target && e.target.matches(`${".offer__slider-next"}, ${".offer__slide__next__img"}`)) {
+
+    //         if (i == 3) {
+    //             i = 0;
+    //             b = 1;
+    //             current.textContent = `${0}${b}`;
+    //             hideSlide();
+    //             showSlide(i);
+    //             sliderCounter.removeEventListener("click", (e));
+    //         } else if (i <= 3) {
+    //             i++;
+    //             b++;                   
+    //             if (i < 10) {
+    //                 current.textContent = `${0}${b}`;
+    //             } else {
+    //                 current.textContent = b;
+    //             }
+    //             hideSlide();
+    //             showSlide(i);
+    //         }
+    //     }
+
+    //     if (e.target && e.target.matches(`${".offer__slider-prev"}, ${".offer__slide__prev__img"}`)) {
+
+    //         if (i == 0) {
+    //             i = 3;
+    //             b = 4;
+    //             current.textContent = `${0}${b}`;
+    //             hideSlide();
+    //             showSlide(i);
+    //             sliderCounter.removeEventListener("click", (e));
+    //         } else if (i <= 3) {
+    //             i--;
+    //             b--;
+
+    //             if (i < 10) {
+    //                 current.textContent = `${0}${b}`;
+    //             } else {
+    //                 current.textContent = b;
+    //             }
+
+    //             hideSlide();
+    //             showSlide(i);
+    //         }
+    //     }
+    // });
+
 
     //Timer
 
